3acac903aaccc23b6b8a45404996c6209728866e
ruggeri <ruggeri_oliveira@hotmail.com>
2023-06-02 12:17:23 -0300

    add metodo para inicializar o modelMapper

--- src/main/java/br/com/crud/biblioteca/BibliotecaApplication.java
+++ src/main/java/br/com/crud/biblioteca/BibliotecaApplication.java
@@ -1,10 +1,17 @@
 package br.com.crud.biblioteca;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BibliotecaApplication {
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BibliotecaApplication.class, args);
